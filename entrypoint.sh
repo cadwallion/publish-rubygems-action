b8b21c36ec884b3ad47707724b30e54330bca267
Andrew Nordman <cadwallion@github.com>
2019-01-15 18:14:32 -0600

    Sets 0600 on Gem Credentials

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,14 @@
 echo "Setting up gem credentials..."
 set +x
 mkdir -p ~/.gem
+
 cat << EOF > ~/.gem/credentials
 ---
 :github: Bearer ${GITHUB_TOKEN}
 :rubygems_api_key: ${RUBYGEMS_API_KEY}
 EOF
+
+chmod 0600 ~/.gem/credentials
 set -x
 
 echo "Installing dependencies..."
